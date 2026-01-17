d9294a17faacb5503ae0ee7f6cf0d0233692ea50
Ian Mabruk <ian@example.com>
2026-01-17 12:51:14 +0300

    initial commit

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,10 @@
 bind = "0.0.0.0:5000"
 workers = 2
-timeout = 120
+worker_class = "sync"
+timeout = 300
 keepalive = 2
 max_requests = 1000
 max_requests_jitter = 100
-preload_app = False
+preload_app = True
+worker_connections = 100
+graceful_timeout = 30
